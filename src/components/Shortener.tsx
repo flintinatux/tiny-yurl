b51762c20f62b0d0436cfb22406045f9ca3f3a69
flintinatux <mccormack.scott@gmail.com>
2025-07-15 15:47:34 -0400

    Better React idioms for forms

--- src/components/Shortener.tsx
+++ src/components/Shortener.tsx
@@ -1,25 +1,29 @@
 'use client'
 
-import { FormEvent, Fragment, useState } from 'react'
+import { Fragment, useState } from 'react'
 
 export default function Shortener() {
+  const [ shortened, setShortened ] = useState<boolean>(false)
   const [ yurl, setYurl ] = useState<string>('')
 
-  const shorten = (e: FormEvent<HTMLFormElement>): void => {
-    e.preventDefault()
-    e.stopPropagation()
-    setYurl((e.target as HTMLFormElement).longYurl.value)
+  const shorten = (): void => {
+    setShortened(true)
+  }
+
+  const updateYurl = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setYurl(event.target.value)
+    setShortened(false)
   }
 
   return (
     <Fragment>
-      <form className="flex mt-2" onSubmit={shorten}>
-        <input className="bg-white grow-2 px-1 py-0.5 rounded-l-lg" name="longYurl" placeholder="Long yurl" type="text"/>
+      <form className="flex mt-2" action={shorten}>
+        <input className="bg-white grow-2 px-1 py-0.5 rounded-l-lg" name="longYurl" onChange={updateYurl} placeholder="Long yurl" type="text" value={yurl}/>
         <button className="bg-blue-700 px-1 py-0.5 rounded-r-lg shrink-0 text-white" type="submit">Shorten</button>
       </form>
 
       {
-        yurl && <div className="mt-2">
+        shortened && <div className="mt-2">
           <p className="text-sm text-gray-500">Shortened yurl:</p>
           <a className="text-blue-700 hover:text-blue-800" href={yurl} target="_blank" rel="noopener noreferrer">
             {yurl}
