526e98b41a83a21639f06ef5008e8beb18701bcb
flintinatux <mccormack.scott@gmail.com>
2025-07-15 16:37:49 -0400

    Client-side url validation

--- src/components/Shortener.tsx
+++ src/components/Shortener.tsx
@@ -1,18 +1,35 @@
 'use client'
 
 import { Fragment, useState } from 'react'
+import * as z from 'zod/mini'
+
+const schema = z.url({
+  protocol: /^https?$/,
+  hostname: z.regexes.domain
+})
 
 export default function Shortener() {
+  const [ error, setError ] = useState<string>('')
   const [ shortened, setShortened ] = useState<boolean>(false)
   const [ yurl, setYurl ] = useState<string>('')
 
-  const shorten = (): void => {
+  const shorten = async (): void => {
+    if (!yurl) return
+
+    try {
+      schema.parse(yurl)
+    } catch {
+      setError('Oops! That\'s an invalid URL. Please double-check your input.')
+      return
+    }
+
     setShortened(true)
   }
 
   const updateYurl = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setYurl(event.target.value)
+    setError('')
     setShortened(false)
+    setYurl(event.target.value)
   }
 
   return (
@@ -21,6 +38,12 @@ export default function Shortener() {
         <input className="bg-white grow-2 px-1 py-0.5 rounded-l-lg" name="longYurl" onChange={updateYurl} placeholder="Long yurl" type="text" value={yurl}/>
         <button className="bg-blue-700 px-1 py-0.5 rounded-r-lg shrink-0 text-white" type="submit">Shorten</button>
       </form>
+
+      {
+        error && <div className="mt-1">
+          <p className="text-sm text-red-500">{error}</p>
+        </div>
+      }
 
       {
         shortened && <div className="mt-2">
