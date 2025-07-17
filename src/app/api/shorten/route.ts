dc32e455a1a0a5bf041a92e8dcd5bb74aa47ed87
flintinatux <mccormack.scott@gmail.com>
2025-07-17 14:28:50 -0400

    Use origin header instead

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,14 +1,6 @@
 import { uid } from 'uid/secure'
 import * as z from 'zod/mini'
 
-const {
-  VERCEL_ENV,
-  VERCEL_PROJECT_PRODUCTION_URL
-} = process.env
-
-const host = VERCEL_PROJECT_PRODUCTION_URL
-const protocol = VERCEL_ENV === 'development' ? 'http://' : 'https://'
-
 const schema = z.object({
   yurl: z.url({
     protocol: /^https?$/,
@@ -21,7 +13,7 @@ const tiny: Record<string, string> = {}
 export const POST = async (request: Request): Promise<Response> => {
   const { yurl } = schema.parse(await request.json())
 
-  tiny[yurl] = tiny[yurl] || `${protocol}${host}/${uid(8)}`
+  tiny[yurl] = tiny[yurl] || `${request.headers.get('origin')}/t/${uid(8)}`
 
   return Response.json({ tiny: tiny[yurl] })
 }
