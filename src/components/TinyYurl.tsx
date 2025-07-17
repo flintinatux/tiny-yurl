2383db134ca42e4cc567b7b5f187c408476a36f5
flintinatux <mccormack.scott@gmail.com>
2025-07-17 13:09:23 -0400

    Loading and error states

--- src/components/TinyYurl.tsx
+++ src/components/TinyYurl.tsx
@@ -1,15 +1,38 @@
 'use client'
 
-// import useTinyYurl from '@/swr/useTinyYurl'
+import useSWR from 'swr'
+
+const fetcher = (yurl: string): Promise<string> =>
+  fetch('/api/shorten', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ yurl })
+  }).then(res => res.json())
 
 export default function TinyYurl({ yurl }: { yurl: string }) {
-  // const { data, error, isLoading } = useTinyYurl(yurl)
+  const { data, error } = useSWR(yurl, fetcher)
+
+  if (error) {
+    return (
+      <div className="mt-2">
+        <p className="text-sm text-red-500">Error shortening yurl: {error.message}</p>
+      </div>
+    )
+  }
+
+  if (!data) {
+    return (
+      <div className="mt-2">
+        <p className="text-sm text-gray-500">Shortening yurl...</p>
+      </div>
+    )
+  }
 
   return (
     <div className="mt-2">
       <p className="text-sm text-gray-500">Shortened yurl:</p>
-      <a className="text-blue-700 hover:text-blue-800" href={yurl} target="_blank" rel="noopener noreferrer">
-        {yurl}
+      <a className="text-blue-700 hover:text-blue-800" href={data.tiny} target="_blank" rel="noopener noreferrer">
+        {data.tiny}
       </a>
     </div>
   )
