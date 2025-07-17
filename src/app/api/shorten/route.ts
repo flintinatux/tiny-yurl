aa984439901c8480fa108c4703c94ef20190e831
flintinatux <mccormack.scott@gmail.com>
2025-07-17 14:22:55 -0400

    Try production url instead

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -3,9 +3,10 @@ import * as z from 'zod/mini'
 
 const {
   VERCEL_ENV,
-  VERCEL_URL
+  VERCEL_PROJECT_PRODUCTION_URL
 } = process.env
 
+const host = VERCEL_PROJECT_PRODUCTION_URL
 const protocol = VERCEL_ENV === 'development' ? 'http://' : 'https://'
 
 const schema = z.object({
@@ -20,7 +21,7 @@ const tiny: Record<string, string> = {}
 export const POST = async (request: Request): Promise<Response> => {
   const { yurl } = schema.parse(await request.json())
 
-  tiny[yurl] = tiny[yurl] || `${protocol}${VERCEL_URL}/${uid(8)}`
+  tiny[yurl] = tiny[yurl] || `${protocol}${host}/${uid(8)}`
 
   return Response.json({ tiny: tiny[yurl] })
 }
